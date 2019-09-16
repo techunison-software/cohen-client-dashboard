f269135c655b3983b0a3788f0d17bf9a9dd7c5d0
AswathTUI <aswath@techunison.com>
2019-09-16 20:07:25 +0530

    Update siteConfig.js
    Removed the testing scripts of semantic-ui

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -81,7 +81,7 @@ const siteConfig = {
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js','node_modules\\semantic-ui\\dist\\semantic.min.js'],
+  scripts: ['https://buttons.github.io/buttons.js'],
 
   // On page navigation for the current documentation page.
   onPageNav: 'separate',
